6dc6268220dfd5b56dd8cde74a582296f1cd44d3
Ilia Koval <TheNovich@yandex.ru>
2022-12-18 12:33:02 +0300

    fix: html(popup and semantics) js css

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,38 @@
-let profileUserName = document.querySelector('.profile__user-name');
-let profileUserAbout = document.querySelector('.profile__user-about');
-let profileEditorButton = document.querySelector('.profile__editor-button');
-let popup = document.querySelector('.popup');
-let popupInputValueName = document.querySelector('.popup__input_value_name');
-let popupInputValueAbout = document.querySelector('.popup__input_value_about');
-let popupForm = document.querySelector('.popup__form');
-let popupButtonValueSubmit = document.querySelector('.popup__button_value_submit');
-let popupButtonValueClose = document.querySelector('.popup__button_value_close');
+const profileUserName = document.querySelector('.profile__user-name');
+const profileUserAbout = document.querySelector('.profile__user-about');
+const profileEditorButton = document.querySelector('.profile__editor-button');
+const popup = document.querySelector('.popup');
+const popupInputValueName = document.querySelector('.popup__input_value_name');
+const popupInputValueAbout = document.querySelector('.popup__input_value_about');
+const popupForm = document.querySelector('.popup__form');
+const popupButtonValueClose = document.querySelector('.popup__button_value_close');
 let elementsElementLikeButton = document.querySelectorAll('.elements__element-like-button');
 
-function openPopup(){
-    popup.classList.toggle('popup_active');
+function openPopup() {
+    popup.classList.add('popup_active');
     popupInputValueName.value = profileUserName.textContent;
     popupInputValueAbout.value = profileUserAbout.textContent;
 }
 
-function submitForm(evt){
+function submitForm(evt) {
     evt.preventDefault();
     profileUserName.textContent = popupInputValueName.value;
     profileUserAbout.textContent = popupInputValueAbout.value;
-    popup.classList.remove('popup_active');
+    closePopup();
 }
 
-function closeForm(){
+function closePopup() {
     popup.classList.remove('popup_active');
-}
-
-function like(e){
-    e.target.classList.toggle('elements__element-like-button_active')
 };
 
 profileEditorButton.addEventListener('click', openPopup);
-popupButtonValueSubmit.addEventListener('click', submitForm);
-popupButtonValueClose.addEventListener('click', closeForm);
+popupForm.addEventListener('submit', submitForm);
+popupButtonValueClose.addEventListener('click', closePopup);
 
-for (i = 0; i <= elementsElementLikeButton.length - 1; i++){
-    elementsElementLikeButton[i].addEventListener('click', like);
-}
+// for (i = 0; i <= elementsElementLikeButton.length - 1; i++){
+//     elementsElementLikeButton[i].addEventListener('click', like);
+// };
+
+// function like(e){
+//     e.target.classList.toggle('elements__element-like-button_active');
+// };
